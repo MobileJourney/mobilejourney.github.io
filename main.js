e7f8297c20c2888aca8eacea90d357c2b6847c53
MobileJourney <45786303+MobileJourney@users.noreply.github.com>
2018-12-12 11:25:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ function initialize() {
     var mp = new BMap.Map("container");
     var point = new BMap.Point(121.491, 31.233);
     var points = [
-        new BMap.Point(-2.349766, 53.465317) 
+        new BMap.Point(53.465/*317*/, -2.349/*766*/) 
+    ];
+    var googlePoints = [
+        
     ];
     for (let i = 0; i < points.length; i++) {
         var marker = new BMap.Marker(points[i]);
@@ -12,7 +15,7 @@ function initialize() {
     function route(){
         alert("works")
     };
-    mp.centerAndZoom(new BMap.Point(31.233, 121.491), 11);
+    mp.centerAndZoom(new BMap.Point(121.491, 31.233), 11);
     mp.enableScrollWheelZoom();
 }
 
