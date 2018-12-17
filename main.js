e199925cb194b3f9b7868eae9ca6d07778344c13
MobileJourney <45786303+MobileJourney@users.noreply.github.com>
2018-12-17 22:43:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -14,12 +14,17 @@ function initialize() {
         for (let i = 0; i < points.length; i++) {
             var marker = new BMap.Marker(points[i]);
             map.addOverlay(marker);
+            marker.addEventListener("click", route);
         }
         var driving = new BMap.DrivingRoute(map, { renderOptions: { map: map, autoViewport: true } });
         driving.search(points[0], points[1]);
     }
     navigator.geolocation.getCurrentPosition(myPos);
    
+    function route(){
+        alert("http://map.baidu.com/?latlng=53.465317,-2.349766&title=%E6%88%91%E7%9A%84%E4%BD%8D%E7%BD%AE&content=%E7%99%BE%E5%BA%A6%E5%A5%8E%E7%A7%91%E5%A4%A7%E5%8E%A6&autoOpen=true&l")
+    };
+    
     map.centerAndZoom(new BMap.Point(-2.349/*766*/, 53.465/*317*/), 11);
     map.enableScrollWheelZoom();
 }
