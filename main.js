6ca6b12986f9a6e962c143bda4234421f2451285
MobileJourney <45786303+MobileJourney@users.noreply.github.com>
2018-12-17 21:16:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 function initialize() {
     var mp = new BMap.Map("container");
-    //var point = new BMap.Point(121.491, 31.233);
     var navigationControl = new BMap.NavigationControl({
         anchor: BMAP_ANCHOR_TOP_LEFT,
         type: BMAP_NAVIGATION_CONTROL_ZOOM
     });
     mp.addControl(navigationControl);
-    var points = [
-        new BMap.Point(-2.349766, 53.465317) 
-    ];
-    for (let i = 0; i < points.length; i++) {
-        var marker = new BMap.Marker(points[i]);
-        mp.addOverlay(marker);
-        marker.addEventListener("click", route);
+    function myPos(position) {
+        console.log(position.coords.latitude, position.coords.longitude);
+        var points = [
+            new BMap.Point(-2.349/*766*/, 53.465/*317*/)
+        ];
+        points.push(new BMap.Point(position.coords.longitude.toFixed(3), position.coords.latitude.toFixed(3)));
+        for (let i = 0; i < points.length; i++) {
+            var marker = new BMap.Marker(points[i]);
+            map.addOverlay(marker);
+        }
+        var driving = new BMap.DrivingRoute(map, { renderOptions: { map: map, autoViewport: true } });
+        driving.search(points[0], points[1]);
     }
-    function route(){
-        alert("works")
-    };
+    navigator.geolocation.getCurrentPosition(myPos);
+   
     mp.centerAndZoom(new BMap.Point(-2.349/*766*/, 53.465/*317*/), 11);
     mp.enableScrollWheelZoom();
 }
