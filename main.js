3d48e1809077b98cfb37feb5283b807ab1dc8cc0
MobileJourney <45786303+MobileJourney@users.noreply.github.com>
2018-12-20 09:44:27 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -5,34 +5,54 @@ function initialize() {
         type: BMAP_NAVIGATION_CONTROL_ZOOM
     });
     map.addControl(navigationControl);
-    function myPos(position) {
-        //console.log(position.coords.latitude, position.coords.longitude);
+    function myPos() {
         var points = [
             new BMap.Point(-2.349/*766*/, 53.465/*317*/),
-            new BMap.Point(-2.549/*766*/, 54.665/*317*/)
-
+            new BMap.Point(-2.549/*766*/, 54.665/*317*/),
+            new BMap.Point(-2.549/*766*/, 52.665/*317*/)
         ];
-        //points.push(new BMap.Point(position.coords.longitude.toFixed(3), position.coords.latitude.toFixed(3)));
         for (let i = 0; i < points.length; i++) {
             var marker = new BMap.Marker(points[i]);
             map.addOverlay(marker);
-            marker.addEventListener("click", route);
+            marker.addEventListener("click", modalWindow);
+            marker.onclick = function () {
+                var link = document.getElementById("link-baidu");
+                link.setAttribute("href", "http://map.baidu.com/?latlng=" + marker.point.lat + "," + marker.point.lng + "&title=%E6%88%91%E7%9A%84%E4%BD%8D%E7%BD%AE&content=%E7%99%BE%E5%BA%A6%E5%A5%8E%E7%A7%91%E5%A4%A7%E5%8E%A6&autoOpen=true&l");
+                //console.log(marker.point.lat + ", " + marker.point.lng);
+                console.log(marker.getPosition())
+            } 
         }
-        var driving = new BMap.DrivingRoute(map, { renderOptions: { map: map, autoViewport: true } });
-        driving.search(points[0], points[1]);
+        //var driving = new BMap.DrivingRoute(map, { renderOptions: { map: map, autoViewport: true } });
+        //driving.search(points[0], points[1]);
     }
-    //navigator.geolocation.getCurrentPosition(myPos);
     myPos();
     
- 
+
    
-    function route(){
+    function modalWindow(){
         modal.classList.remove("closed");
         modalOverlay.classList.remove("closed");
     };
     
     map.centerAndZoom(new BMap.Point(-2.349766, 53.465317), 11);
     map.enableScrollWheelZoom();
+    /* map.addEventListener("click", function (e) {
+        alert(e.point.lng + ", " + e.point.lat);
+    }); */
+    
+    if(!navigator.geolocation){
+        console.log("Geolocation not supported")
+        return
+    }
+    function succes(position){
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        console.log(latitude, longitude)
+    }
+    function error(){
+        console.log("Unable to get your location")
+    }
+    navigator.geolocation.getCurrentPosition(succes, error);
 }
 
 function loadScript() {
