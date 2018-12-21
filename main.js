0653ba48091e2034dc6711349b71f0a429680ed1
MobileJourney <45786303+MobileJourney@users.noreply.github.com>
2018-12-21 12:25:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,19 +11,18 @@ function initialize() {
             new BMap.Point(-2.549/*766*/, 54.665/*317*/),
             new BMap.Point(-2.549/*766*/, 52.665/*317*/)
         ];
+
         for (let i = 0; i < points.length; i++) {
             var marker = new BMap.Marker(points[i]);
             map.addOverlay(marker);
             marker.addEventListener("click", modalWindow);
-            marker.onclick = function () {
+            marker.onclick = function (e) {
                 var link = document.getElementById("link-baidu");
-                link.setAttribute("href", "http://map.baidu.com/?latlng=" + marker.point.lat + "," + marker.point.lng + "&title=%E6%88%91%E7%9A%84%E4%BD%8D%E7%BD%AE&content=%E7%99%BE%E5%BA%A6%E5%A5%8E%E7%A7%91%E5%A4%A7%E5%8E%A6&autoOpen=true&l");
-                //console.log(marker.point.lat + ", " + marker.point.lng);
-                console.log(marker.getPosition())
-            } 
+                var p = e.target;
+                link.setAttribute("href", "http://map.baidu.com/?latlng=" + p.getPosition().lat + "," + p.getPosition().lng + "&title=%E6%88%91%E7%9A%84%E4%BD%8D%E7%BD%AE&content=%E7%99%BE%E5%BA%A6%E5%A5%8E%E7%A7%91%E5%A4%A7%E5%8E%A6&autoOpen=true&l");
+                
+            } //http://api.map.baidu.com/marker?location=40.047669,116.313082&title=我的位置&content=百度奎科大厦&output=html&src=webapp.baidu.openAPIdemo
         }
-        //var driving = new BMap.DrivingRoute(map, { renderOptions: { map: map, autoViewport: true } });
-        //driving.search(points[0], points[1]);
     }
     myPos();
     
@@ -34,20 +33,24 @@ function initialize() {
         modalOverlay.classList.remove("closed");
     };
     
-    map.centerAndZoom(new BMap.Point(-2.349766, 53.465317), 11);
+    map.centerAndZoom(new BMap.Point(-2.349766, 53.465317), 8);
+    
+    //map.setCenter(new BMap.Point(-2.549/*766*/, 52.665/*317*/));
     map.enableScrollWheelZoom();
-    /* map.addEventListener("click", function (e) {
-        alert(e.point.lng + ", " + e.point.lat);
-    }); */
     
     if(!navigator.geolocation){
-        console.log("Geolocation not supported")
+        console.log("Geolocation not supported");
         return
     }
     function succes(position){
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
-        console.log(latitude, longitude)
+        console.log(latitude, longitude);
+        var myLocation = new BMap.Point(longitude, latitude);
+        var myMarker = new BMap.Marker(myLocation);
+        myMarker.setAnimation(BMAP_ANIMATION_BOUNCE)
+
+        map.addOverlay(myMarker)
     }
     function error(){
         console.log("Unable to get your location")
